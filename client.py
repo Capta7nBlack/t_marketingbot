aaf58471cddbcbdfcfcb97fdcf2670b2a9654697
Capta7nBlack <sayat.mushkin@nu.edu.kz>
2025-02-08 20:47:25 +0500

    Introduced dictionary for the arguments that has to be passed between functions

--- client.py
+++ client.py
@@ -29,11 +29,12 @@ def start(m):
 
 @bot.message_handler(func=lambda message: message.text == 'Создать рекламный пост')
 def create_post(m):
+    data = {}
     bot.send_message(m.chat.id,
                      f"Отправьте фото для поста.")
-    bot.register_next_step_handler(m, photo_handler)
+    bot.register_next_step_handler(m, photo_handler, data)
 
-def photo_handler(m):
+def photo_handler(m, data):
     if m.content_type != 'photo':
         # Inform the user that a photo is required and re-register the handler
         msg = bot.send_message(m.chat.id, "Пожалуйста отправьте Фото.")
@@ -51,15 +52,17 @@ def photo_handler(m):
     with open(save_input_path, "wb") as new_file:
         new_file.write(downloaded_file)
     bot.send_message(m.chat.id, f"Фото принято и обрабатывается. Пожалуйста отправьте текст для помещения на фото.")
-    bot.register_next_step_handler(m, lambda m: photo_text_handler(m, path = absolute_path(save_input_path)))
+    data['input_photo_path'] = absolute_path(save_input_path)
+    bot.register_next_step_handler(m, photo_text_handler, data)
 
-def photo_text_handler(m, path):
-    absolute_input_path = path
+def photo_text_handler(m, data):
+
+    absolute_input_path = data.get('input_photo_path')
     if m.content_type != 'text':
         # Inform the user that a photo is required and re-register the handler
         msg = bot.send_message(m.chat.id, "Пожалуйста отправьте Текст")
 
-        bot.register_next_step_handler(msg, lambda m: photo_text_handler(m, path = save_path))
+        bot.register_next_step_handler(msg, photo_text_handler, data)
         return
     output_path_file = output_absolute_folder + f"/{m.from_user.first_name}_{m.text}_edited.png"
     overlay_images(absolute_input_path, frame_absolute_path, output_path_file, m.text) 
@@ -68,7 +71,8 @@ def photo_text_handler(m, path):
         bot.send_message(m.chat.id,
                     f"Отправьте текст который будет под постом."
                      )
-        bot.register_next_step_handler(m, lambda m: post_text_handler(m, photo_path = output_path_file))
+        data['output_photo_path'] = output_path_file
+        bot.register_next_step_handler(m, post_text_handler, data)
     else:
         with open(output_path_file, "rb") as photo:
             bot.send_photo(m.chat.id, photo)
@@ -77,9 +81,11 @@ def photo_text_handler(m, path):
                      f"Это то, как будет выглядеть ваш пост. Если вам хотите изменить фото или текст, начните процесс сначалo.",
                          reply_markup = markup_verification()
                      )
-        bot.register_next_step_handler(m,lambda m: verification_handler(m, photo_path = output_path_file, post_text = None))
+        data['post_text'] = None
+        bot.register_next_step_handler(m,verification_handler, data)
 
-def post_text_handler(m, photo_path):
+def post_text_handler(m, data):
+    photo_path = data.get('output_photo_path')
     with open(photo_path, "rb") as photo:
         bot.send_photo(m.chat.id, photo, caption = m.text)
 
@@ -87,11 +93,12 @@ def post_text_handler(m, photo_path):
                      f"Это то, как будет выглядеть ваш пост. Если вам хотите изменить фото или текст, начните процесс сначал.",
                      reply_markup = markup_verification()
                      )
-    bot.register_next_step_handler(m, lambda m: verification_handler(m, photo_path = photo_path, post_text = m.text))
+    data['post_text'] = m.text
+    bot.register_next_step_handler(m, verification_handler, data)
 
-def verification_handler(m, photo_path, post_text):
+def verification_handler(m, data):
     # Здесь должен сохранить фото и текст под пост в дазу банных
     pass
-
+    
 
 bot.polling()
